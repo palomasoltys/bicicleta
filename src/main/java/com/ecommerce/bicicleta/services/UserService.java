3aef460366317a399d63d2b8c80ec52f3eea7cd6
palomasoltys <pfs601@gmail.com>
2022-12-07 12:29:13 -0500

    Create UserService and implement findById and findAll (Users)

---
+++ src/main/java/com/ecommerce/bicicleta/services/UserService.java
@@ -0,0 +1,26 @@
+package com.ecommerce.bicicleta.services;
+
+import com.ecommerce.bicicleta.entities.User;
+import com.ecommerce.bicicleta.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}
