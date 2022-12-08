f938c101315bcbd93edaa895b2379892d97e9715
palomasoltys <pfs601@gmail.com>
2022-12-08 12:54:08 -0500

    Create OrderItemPK and OrderItem (many to many relationship)

---
+++ src/main/java/com/ecommerce/bicicleta/entities/OrderItem.java
@@ -0,0 +1,83 @@
+package com.ecommerce.bicicleta.entities;
+
+import com.ecommerce.bicicleta.entities.pk.OrderItemPK;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    @Column
+    private Integer quantity;
+
+    @Column
+    private Double price;
+
+    public OrderItem() {
+
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        this.quantity = quantity;
+        id.setOrder(order);
+        id.setProduct(product);
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return id.equals(orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
