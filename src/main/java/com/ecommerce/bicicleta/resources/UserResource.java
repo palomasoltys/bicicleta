1e8b9325735febe33e5df84aa709a75cfe4204da
palomasoltys <pfs601@gmail.com>
2022-12-08 21:51:11 -0500

    Create User delete

--- src/main/java/com/ecommerce/bicicleta/resources/UserResource.java
+++ src/main/java/com/ecommerce/bicicleta/resources/UserResource.java
@@ -38,4 +38,10 @@ public class UserResource {
 
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
