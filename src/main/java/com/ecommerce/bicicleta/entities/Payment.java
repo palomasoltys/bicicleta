2f128119f3be266b4dc4a76752c3d76af27e73a9
palomasoltys <pfs601@gmail.com>
2022-12-08 21:30:12 -0500

    Create subTotal and Total Methods for OrderItem and Order

--- src/main/java/com/ecommerce/bicicleta/entities/Payment.java
+++ src/main/java/com/ecommerce/bicicleta/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.ecommerce.bicicleta.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -18,6 +19,7 @@ public class Payment implements Serializable {
     private Long id;
     @Column
     private Instant moment;
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
