984ece1f9e56b69076dbacb5f9ebf6e608a4de71
palomasoltys <pfs601@gmail.com>
2022-12-19 20:03:26 -0500

    Switch from @RestController to Controller and fix register endpoint

--- src/main/java/com/ecommerce/bicicleta/resources/UserResource.java
+++ src/main/java/com/ecommerce/bicicleta/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.ecommerce.bicicleta.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,7 +14,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(value = "/users")
 public class UserResource {
 
