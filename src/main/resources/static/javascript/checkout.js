13e1e9e6c131287c588ae74030cc947a657c6b4d
palomasoltys <pfs601@gmail.com>
2023-01-05 13:39:31 -0500

    Create remove and add items to the cart button and add an event listener that updates itemQuantity

--- src/main/resources/static/javascript/checkout.js
+++ src/main/resources/static/javascript/checkout.js
@@ -1,5 +1,51 @@
-const itemQuantity = document.getElementById('item-quantity').innerText;
+let itemQuantity = document.getElementById('item-quantity').innerText;
 const itemPrice = document.getElementById('item-price').innerText;
 const itemSubTotal = document.getElementById('item-subtotal').innerText;
 const productName = document.getElementById('product-name').innerText;
 const productDescription = document.getElementById('product-description').innerText;
+const orderId = document.getElementById('order-id').innerText;
+let url = window.location.href;
+let productId = url.substring(url.lastIndexOf('/') + 1);
+
+const baseUrl = 'http://localhost:8080/products/cart'
+
+const headers = {
+    'Content-Type':'application/json'
+}
+
+const removeItemBtn = document.getElementById('remove-item-btn');
+
+
+const addItemBtn = document.getElementById('add-item-btn');
+
+
+function updateCart(operator) {
+    if(operator === "+") {
+        itemQuantity++
+    } else if (operator === "-"){
+        itemQuantity--
+    }
+
+    let obj = {
+    id: orderId,
+    quantity: itemQuantity
+    }
+//
+//    const response = await fetch(`${baseUrl}/update-cart/${productId}`, {
+//    method: "POST",
+//    body: JSON.stringify(obj),
+//    headers: headers
+//    })
+//    console.log(response)
+}
+
+const checkoutItemsBtn = document.getElementById('checkout-btn');
+
+//checkoutItemsBtn.addEventListener('click', () => {
+//})
+
+removeItemBtn.addEventListener("click", () => { updateCart("-") });
+addItemBtn.addEventListener("click", () => { updateCart("+") });
+
+
+
