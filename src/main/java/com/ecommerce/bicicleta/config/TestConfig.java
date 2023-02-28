afa06b9c1caa9b39f7a2c6eab8657915dd3e3fe2
palomasoltys <pfs601@gmail.com>
2023-02-28 10:32:47 -0500

    Update test objects

--- src/main/java/com/ecommerce/bicicleta/config/TestConfig.java
+++ src/main/java/com/ecommerce/bicicleta/config/TestConfig.java
@@ -118,7 +118,7 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "nick@gmail.com", "654321", "Nick");
         User u3 = new User(null, "bal@gmail.com", "123", "Bal");
 
-        Order o1 = new Order(null, Instant.parse("2023-02-15T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o1 = new Order(null, Instant.parse("2023-02-15T19:53:07Z"), OrderStatus.SHIPPED, u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u3);
 
